e25a86ecce70d207734ffdd606375d5feaca0f7c
Pinal Bhatt <Pinal.Bhatt@ww.com>
2020-04-12 16:25:28 -0400

    pkg versions

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 /* eslint no-console: 0 */
 const core = require('@actions/core');
-const wait = require('./lib/wait');
-
+// const wait = require('./lib/wait');
+const { getPackageVersion, getPackageVersionTag } = require('./lib/pkg-version');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`);
+    const pkgJsonLocation = core.getInput('pkgJsonLocation');
+    // const ms = core.getInput('milliseconds');
+    console.log(`Reading package.json at ${pkgJsonLocation}`);
 
-    core.debug((new Date()).toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString());
+    const packageVersion = getPackageVersion(pkgJsonLocation);
+    const packageVersionTag = getPackageVersionTag(pkgJsonLocation);
+    // core.debug((new Date()).toTimeString());
+    // await wait(parseInt(ms, 10));
+    // / core.debug((new Date()).toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
+    // core.setOutput('time', new Date().toTimeString());
+    core.setOutput('packageVersion', packageVersion);
+    core.setOutput('packageVersionTag', packageVersionTag);
   } catch (error) {
     core.setFailed(error.message);
   }
