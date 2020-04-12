15fc4657ee7e78da450455d6890d7adde37c9c0b
Pinal Bhatt <Pinal.Bhatt@ww.com>
2020-04-12 16:52:05 -0400

    v0.2

--- test/pkg-version.test.js
+++ test/pkg-version.test.js
@@ -17,6 +17,6 @@ test('getPackageVersionTag with snapshot case-1', () => {
 });
 
 test('getPackageVersionTag with snapshot case-2', () => {
-    const tag = getPackageVersionTag('20.6.0-SNAPSHOT');
-    expect(tag).toBe('20.6.0-SNAPSHOT');
+  const tag = getPackageVersionTag('20.6.0-SNAPSHOT');
+  expect(tag).toBe('20.6.0-SNAPSHOT');
 });
